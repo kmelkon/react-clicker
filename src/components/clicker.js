e014f36abef607369321cd636fa595c6722c29d9
karam <kmelkon@gmail.com>
2018-11-27 15:35:27 +0100

    added basic clicker functions

--- src/components/clicker.js
+++ src/components/clicker.js
@@ -4,14 +4,16 @@ import { Button, Pane } from "evergreen-ui";
 import "./clicker.css";
 
 // implement achievements
-// implement automatic clickers and their prices
+// implement automatic clickers' prices
 // implement upgrades and their prices
+// move automatic clicker into its own component
 
 export default class Clicker extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      btnClicks: 0,
+      auto: null
     };
     this.addOne = this.addOne.bind(this);
     this.addTen = this.addTen.bind(this);
@@ -20,37 +22,49 @@ export default class Clicker extends React.Component {
 
   addOne() {
     this.setState({
-      counter: this.state.counter + 1
+      btnClicks: this.state.btnClicks + 1
     });
   }
 
   addTen() {
     this.setState({
-      counter: this.state.counter + 10
+      btnClicks: this.state.btnClicks + 10
     });
   }
 
   addHundred() {
     this.setState({
-      counter: this.state.counter + 100
+      btnClicks: this.state.btnClicks + 100
     });
   }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      if (this.state.auto) {
+        this.setState({ btnClicks: this.state.btnClicks + 1 });
+      }
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
     return (
       <div>
         <Button marginBottom={20} height={56} onClick={this.addOne}>
           Click me!
         </Button>
-        <div>{this.state.counter}</div>
+        <div>{this.state.btnClicks}</div>
 
         <div>
-          {this.state.counter >= 10 ? (
+          {this.state.btnClicks >= 10 ? (
             <Button
               marginBottom={20}
               marginTop={10}
               height={40}
-              onClick={this.addTen}
-            >
+              onClick={this.addTen}>
               +10
             </Button>
           ) : (
@@ -58,14 +72,13 @@ export default class Clicker extends React.Component {
               marginBottom={20}
               marginTop={10}
               height={40}
-              disabled="true"
-            >
+              disabled="true">
               +10
             </Button>
           )}
         </div>
         <div>
-          {this.state.counter >= 100 ? (
+          {this.state.btnClicks >= 100 ? (
             <Button marginBottom={20} height={40} onClick={this.addHundred}>
               +100
             </Button>
@@ -75,6 +88,45 @@ export default class Clicker extends React.Component {
             </Button>
           )}
         </div>
+
+        {this.state.btnClicks >= 50 ? (
+          <Button onClick={() => this.setState({ auto: "one" })}>
+            auto click +1
+          </Button>
+        ) : (
+          <Button
+            disabled="true"
+            onClick={() => this.setState({ auto: "one" })}>
+            auto click +1
+          </Button>
+        )}
+      </div>
+    );
+  }
+}
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { number: 0 };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      if (this.props.auto) {
+        this.setState({ number: this.state.number + 1 });
+      }
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.number}</h1>
       </div>
     );
   }
