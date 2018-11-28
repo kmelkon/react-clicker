426285569f73c59d239edc5938ec14b58a3f4585
karam <kmelkon@gmail.com>
2018-11-28 12:36:49 +0100

    Added auto click functionality

--- src/components/clicker.js
+++ src/components/clicker.js
@@ -7,17 +7,36 @@ import "./clicker.css";
 // implement automatic clickers' prices
 // implement upgrades and their prices
 // move automatic clicker into its own component
+// add more resources or more upgrades?
 
 export default class Clicker extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       btnClicks: 0,
-      auto: null
+      auto: null,
+      autoClickNumber: 0,
+      interval: 2000
     };
     this.addOne = this.addOne.bind(this);
     this.addTen = this.addTen.bind(this);
     this.addHundred = this.addHundred.bind(this);
+    this.buyAutoclick = this.buyAutoclick.bind(this);
+    this.shortenAutoClickIntervall = this.shortenAutoClickIntervall.bind(this);
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      if (this.state.auto) {
+        this.setState({
+          btnClicks: this.state.btnClicks + this.state.autoClickNumber
+        });
+      }
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   addOne() {
@@ -38,16 +57,19 @@ export default class Clicker extends React.Component {
     });
   }
 
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      if (this.state.auto) {
-        this.setState({ btnClicks: this.state.btnClicks + 1 });
-      }
-    }, 1000);
+  buyAutoclick() {
+    this.setState({
+      auto: "one",
+      autoClickNumber: this.state.autoClickNumber + 1,
+      btnClicks: this.state.btnClicks - 25
+    });
   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
+  shortenAutoClickIntervall() {
+    this.setState({
+      interval: Math.round(this.state.interval / 2),
+      btnClicks: this.state.btnClicks - 150
+    });
   }
 
   render() {
@@ -64,7 +86,8 @@ export default class Clicker extends React.Component {
               marginBottom={20}
               marginTop={10}
               height={40}
-              onClick={this.addTen}>
+              onClick={this.addTen}
+            >
               +10
             </Button>
           ) : (
@@ -72,7 +95,8 @@ export default class Clicker extends React.Component {
               marginBottom={20}
               marginTop={10}
               height={40}
-              disabled="true">
+              disabled="true"
+            >
               +10
             </Button>
           )}
@@ -90,16 +114,26 @@ export default class Clicker extends React.Component {
         </div>
 
         {this.state.btnClicks >= 50 ? (
-          <Button onClick={() => this.setState({ auto: "one" })}>
-            auto click +1
+          <Button onClick={this.buyAutoclick}>
+            auto click +1 (cost: 25 clicks)
           </Button>
         ) : (
-          <Button
-            disabled="true"
-            onClick={() => this.setState({ auto: "one" })}>
-            auto click +1
+          <Button disabled="true" onClick={this.buyAutoclick}>
+            auto click +1(cost: 25 clicks)
           </Button>
         )}
+
+        <div>
+          {this.state.btnClicks >= 150 && this.state.auto === "one" ? (
+            <Button onClick={this.shortenAutoClickIntervall}>
+              Shorten auto click interval (Cost: 150)
+            </Button>
+          ) : (
+            <Button disabled="true" onClick={this.shortenAutoClickIntervall}>
+              Shorten autoClick interval
+            </Button>
+          )}
+        </div>
       </div>
     );
   }
