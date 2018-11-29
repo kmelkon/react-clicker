3a4edc36795cb51707acd6b75e78be6c048fe80b
Karam Malkon <karam.malkon@snowsoftware.com>
2018-11-29 15:50:02 +0100

    refactored code and added better autoclicker

--- src/components/clicker.js
+++ src/components/clicker.js
@@ -4,33 +4,46 @@ import ReactInterval from "react-interval";
 
 import "./clicker.css";
 
-// implement achievements
-// implement upgrades and their prices
-// move automatic clicker into its own component
 // add more resources or more upgrades?
 
 export default class Clicker extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      // main resource
       btnClicks: 0,
+      // main button action & price
       mainClickIncrement: 1,
       mainClickBuffPrice: 25,
-      auto: null,
+      // cheap autoclicker counter & price
       autoClickNumber: 0,
       autoClickPrice: 25,
+      // better autoclicker counter & price
+      betterAutoClickNumber: 0,
+      betterAutoClickPrice: 50,
+      // not used yet
       interval: 2000,
+      // cheap react interval values
       timer: null,
       enabled: false,
       timeout: 1000,
-      numberOfTimers: 0
-      // callback: this.mainClick
+      numberOfTimers: 0,
+      // better react interval values
+      betterTimer: null,
+      betterEnabled: false,
+      betterTimeout: 800,
+      numberOfBetterTimers: 0,
+      // 50 achievement bool
+      wereFifty: false,
+      // not used yet
+      auto: null
     };
     // React antics
     this.mainClick = this.mainClick.bind(this);
     this.addTen = this.addTen.bind(this);
     this.addHundred = this.addHundred.bind(this);
     this.buyAutoclick = this.buyAutoclick.bind(this);
+    this.buyBetterAutoclick = this.buyBetterAutoclick.bind(this);
     this.shortenAutoClickIntervall = this.shortenAutoClickIntervall.bind(this);
     this.buffMainClick = this.buffMainClick.bind(this);
     this.toast = this.toast.bind(this);
@@ -78,14 +91,25 @@ export default class Clicker extends React.Component {
     });
   }
 
+  // Cheapest auto clicker
   buyAutoclick() {
     this.setState({
-      auto: "one",
       autoClickNumber: this.state.autoClickNumber + 1,
       btnClicks: this.state.btnClicks - this.state.autoClickPrice,
       autoClickPrice: this.state.autoClickPrice + 5,
       enabled: true,
       numberOfTimers: this.state.numberOfTimers + 1
+    });
+  }
+
+  // Hipster auto clicker
+  buyBetterAutoclick() {
+    this.setState({
+      betterAutoClickNumber: this.state.betterAutoClickNumber + 1,
+      btnClicks: this.state.btnClicks - this.state.betterAutoClickPrice,
+      betterAutoClickPrice: this.state.betterAutoClickPrice + 15,
+      betterEnabled: true,
+      numberOfBetterTimers: this.state.numberOfBetterTimers + 1
     });
   }
 
@@ -99,17 +123,33 @@ export default class Clicker extends React.Component {
 
   toast() {
     // Change this to be conditional and dynamic
-    toaster.success("Congrats! You're good at clicking virtual buttons!");
+    console.log(this.state.wereFifty);
+    this.setState({
+      wereFifty: true
+    });
+    if (this.state.wereFifty) {
+      toaster.success("Congrats! You're good at clicking virtual buttons!");
+    }
   }
 
   render() {
-    // use this style of defining shit from state
     let timers = [];
+    // Add keys to these arrays
     for (let i = 0; i < this.state.numberOfTimers; i++) {
       timers.push(
         <ReactInterval
           timeout={this.state.timeout}
           enabled={this.state.enabled}
+          callback={this.mainClick}
+        />
+      );
+    }
+    let betterTimers = [];
+    for (let i = 0; i < this.state.numberOfBetterTimers; i++) {
+      betterTimers.push(
+        <ReactInterval
+          timeout={this.state.betterTimeout}
+          enabled={this.state.betterEnabled}
           callback={this.mainClick}
         />
       );
@@ -123,83 +163,73 @@ export default class Clicker extends React.Component {
 
         {/* we need a way to make autoclickers number dynamic */}
         {timers}
+        {betterTimers}
 
+        <h3>Cheats!!1!</h3>
         <div>
-          {this.state.btnClicks >= 10 ? (
-            <Button
-              marginBottom={20}
-              marginTop={10}
-              height={40}
-              onClick={this.addTen}
-            >
-              +10
-            </Button>
-          ) : (
-            <Button
-              marginBottom={20}
-              marginTop={10}
-              height={40}
-              disabled={true}
-            >
-              +10
-            </Button>
-          )}
+          <Button
+            marginBottom={20}
+            marginTop={10}
+            height={40}
+            onClick={this.addTen}
+            // disabled={!(this.state.btnClicks >= 10)}
+          >
+            +10
+          </Button>
         </div>
         <div>
-          {this.state.btnClicks >= 100 ? (
-            <Button marginBottom={20} height={40} onClick={this.addHundred}>
-              +100
-            </Button>
-          ) : (
-            <Button marginBottom={20} height={40} disabled={true}>
-              +100
-            </Button>
-          )}
+          <Button
+            marginBottom={20}
+            height={40}
+            onClick={this.addHundred}
+            // disabled={!(this.state.btnClicks >= 100)}
+          >
+            +100
+          </Button>
         </div>
 
+        <h3>Store</h3>
         <div>
-          {this.state.btnClicks >= this.state.autoClickPrice ? (
+          <div>
             <Button
               marginBottom={20}
               marginTop={10}
               onClick={this.buyAutoclick}
+              // we enable this button only when this statement is true
+              disabled={!(this.state.btnClicks >= this.state.autoClickPrice)}
             >
               auto click +1 (cost: {this.state.autoClickPrice} clicks)
             </Button>
-          ) : (
+          </div>
+          <div>
             <Button
               marginBottom={20}
               marginTop={10}
-              disabled={true}
-              onClick={this.buyAutoclick}
+              onClick={this.buyBetterAutoclick}
+              // we enable this button only when this statement is true
+              disabled={
+                !(this.state.btnClicks >= this.state.betterAutoClickPrice)
+              }
             >
-              auto click +1 (cost: {this.state.autoClickPrice} clicks)
+              BETTER auto click +10 (cost: {this.state.betterAutoClickPrice}{" "}
+              clicks)
             </Button>
-          )}
+          </div>
         </div>
 
+        <h3>Upgrades</h3>
         <div>
-          {this.state.btnClicks >= this.state.mainClickBuffPrice ? (
-            <Button
-              marginBottom={20}
-              marginTop={10}
-              onClick={this.buffMainClick}
-            >
-              Buff MAIN CLICK (cost: {this.state.mainClickBuffPrice} clicks)
-            </Button>
-          ) : (
-            <Button
-              marginBottom={20}
-              marginTop={10}
-              disabled={true}
-              onClick={this.buffMainClick}
-            >
-              Buff MAIN CLICK (cost: {this.state.mainClickBuffPrice} clicks)
-            </Button>
-          )}
+          <Button
+            marginBottom={20}
+            marginTop={10}
+            onClick={this.buffMainClick}
+            disabled={!(this.state.btnClicks >= this.state.mainClickBuffPrice)}
+          >
+            Buff MAIN CLICK (cost: {this.state.mainClickBuffPrice} clicks)
+          </Button>
         </div>
 
-        <div>
+        {/* <div>
           {this.state.btnClicks >= 150 && this.state.auto === "one" ? (
             <Button
               marginBottom={20}
@@ -218,8 +248,8 @@ export default class Clicker extends React.Component {
               Shorten autoClick interval
             </Button>
           )}
-        </div>
-        {this.state.btnClicks === 50 ? this.toast() : ""}
+        </div> */}
+        {/* {this.state.btnClicks >= 50 ? this.toast() : ""} */}
       </div>
     );
   }
